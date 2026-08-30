90de42aaf34833b78b1d987f23d5e93460fba0dc
Ro'zimurod Shoaxmatov <shoaxmatovrozimurod@gmail.com>
2026-08-30 15:31:37 +0500

    algoritim

---
+++ js/index.js
@@ -0,0 +1,100 @@
+// 1
+// let R = 5;
+// let L = 2 * Math.PI * R;
+// let S = Math.PI * Math.pow (R,2);
+// console.log("Doira radyusi R =", R);
+// console.log("Doira uzunligi L =" , L.toFixed(0));
+// console.log("Doira yuzi S =" , S.toFixed(0));
+
+// 2
+// let a = 16;
+// let b = 4;
+// let G = Math.sqrt(a * b)
+// alert("Orta geometrigi G=" + G)
+
+// 3
+// let x = prompt("x ning qiymatini kiriting:");
+// x = Number (x);
+// let y = 3 * (x ** 6) - 6 * (x ** 2) - 7 ;
+// alert("y ning qiymati:" + y.toFixed(2));
+
+// 4
+// let X = +prompt("necha kg konfet oldingiz X:");
+// let A = +prompt("necha so'm bo'ldi A:");
+// let Y = +prompt("Sizga yana qancha konfet olmoqchisiz Y:");
+
+// let birKgNarxi = A / X;
+// let YKgningNarxi = birKgNarxi * Y;
+
+
+// console.log(YKgningNarxi)
+
+// 5
+// let A = prompt("A sonni kiriting:");
+// let B = prompt("B sonni kiriting:");
+
+// [A, B] = [B, A]
+
+// alert("Almashtirilgandan so'ng:\nA = " + A + "\nB = " + B);
+
+
+
+
+
+// let ism = "Ro'zimurod";
+// const yil = 2007 ;
+
+// console.log(ism , yil);
+
+
+
+// let telegram = {
+//     turi: "Aka",
+//     yaratuchisi:"P Durov",
+//     yaratilgan: 2017,
+//     joyi: "Rossiya",
+//     foydalanuchilari: "Internet foydalanuchilarining 18%",
+//     foydali: "Tanishlar bilan aloqa qilish uchun"
+// };
+// console.log(telegram.yaratuchisi , telegram.foydalanuchilari);
+
+
+// let number = 14567;
+
+// let newNumber = Math.floor ((number / 100) % 10);
+// console.log (newNumber);
+
+// 8,30
+
+// let R = +prompt("R ni kiriting");
+// let L = 2 * Math.PI * R;
+// let S = Math.PI * R * R
+// console.log ("doira uzunligi L =", L .toFixed(2), "\ndoira yuzi S =", S.toFixed(2));
+
+// let a = +prompt("a ni kirit");
+// let b = +prompt("b ni kirit");
+// let G = Math.sqrt(a * b);
+// console.log  (G .toFixed(2));
+
+// let x = +prompt("x ni kirit") ;
+// let y = Math.round(3 * (x ** 6) - 6 * (x ** 2) - 7);
+// // let y = 3 * (x ** 6) - 6 * (x ** 2) - 7;
+
+// console.log( "Funksiya qiymati y =", y);
+
+
+// let x = +prompt("x kg kirit");
+// let a = +prompt("x kg ni narxi");
+// let y = +prompt("qo'shimcha y kg konfet");
+
+// let narxi = a / x;
+// let ykgNarxi = narxi * y;
+// console.log(ykgNarxi)
+
+// let a = +prompt("a ni kirit");
+// let b = +prompt("b ni kirit");
+
+// [a, b] = [b, a];
+
+// console.log("Almashgandan so'ng: \n a = " + a + "\n b = " + b  )
+
